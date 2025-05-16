833b9992ee28513f1d5b254fe68a4ce381cf6b57
AnisLahouar <anislahouar98@gmail.com>
2025-05-16 01:36:30 +0100

    feat: add userMiddleware

--- app/middlewares/user.middleware.js
+++ app/middlewares/user.middleware.js
@@ -1,8 +1,35 @@
 const { HTTP_STATUS } = require("../core");
 const { RES_MESSAGES, JWTSECRET } = require("../core/variables.constants");
+const { User } = require("../database");
 const { checkToken } = require("../helpers/jwt.helper");
 const { logErrorResponse } = require("../helpers/log.helper");
 const ResHandler = require("../helpers/responseHandler.helper");
+
+exports.userIdMiddleware = async (req, res, next) => {
+  const token = req.headers["authorization"];
+  const resHandler = new ResHandler();
+  if (!token) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.NO_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.NO_TOKEN);
+    return resHandler.send(res);
+  }
+  try {
+    const decoded = checkToken(token, JWTSECRET);
+    const { id } = decoded;
+    req.id = id;
+    next();
+  } catch (error) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN);
+    return resHandler.send(res);
+  }
+};
 
 exports.userMiddleware = async (req, res, next) => {
   const token = req.headers["authorization"];
@@ -18,7 +45,8 @@ exports.userMiddleware = async (req, res, next) => {
   try {
     const decoded = checkToken(token, JWTSECRET);
     const { id } = decoded;
-    req.id = id;
+    const user = await User.findByPk(id);
+    req.user = user;
     next();
   } catch (error) {
     resHandler.setError(
