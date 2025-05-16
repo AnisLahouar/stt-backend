7bcbdd02c1036d3b493dbcf23bbd8da42505e5a8
AnisLahouar <anislahouar98@gmail.com>
2025-05-16 01:36:02 +0100

    feat: add roles middleware

--- app/middlewares/role.middleware.js
+++ app/middlewares/role.middleware.js
@@ -1,1 +1,105 @@
 // admin api check?
+
+
+exports.createUserRoleMiddleware = ({
+  roles
+}) => {
+  return (req, res, next) => {
+    try {
+      const userRole = req.user?.role;
+      if (roles.includes(userRole)) {
+        return next();
+      }
+      throw new Error("ROLE MISMATCH")
+
+    } catch (error) {
+      resHandler.setError(
+        HTTP_STATUS.UNAUTHORIZED,
+        RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN
+      );
+      logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN);
+      return resHandler.send(res);
+    }
+  }
+};
+
+
+exports.isAdminMiddleware = async (req, res, next) => {
+  const token = req.headers["authorization"];
+  const resHandler = new ResHandler();
+  if (!token) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.NO_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.NO_TOKEN);
+    return resHandler.send(res);
+  }
+  try {
+    if (req.user && req.user.role == 'admin') {
+      next();
+    } else
+      throw new Error("ROLE MISMATCH")
+  } catch (error) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN);
+    return resHandler.send(res);
+  }
+};
+
+
+exports.isClientMiddleware = async (req, res, next) => {
+  const token = req.headers["authorization"];
+  const resHandler = new ResHandler();
+  if (!token) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.NO_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.NO_TOKEN);
+    return resHandler.send(res);
+  }
+  try {
+    if (req.user && req.user.role == 'client') {
+      next();
+    } else
+      throw new Error("ROLE MISMATCH")
+  } catch (error) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN);
+    return resHandler.send(res);
+  }
+};
+
+
+exports.isOwnerMiddleware = async (req, res, next) => {
+  const token = req.headers["authorization"];
+  const resHandler = new ResHandler();
+  if (!token) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.NO_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.NO_TOKEN);
+    return resHandler.send(res);
+  }
+  try {
+    if (req.user && req.user.role == 'owner') {
+      next();
+    } else
+      throw new Error("ROLE MISMATCH")
+  } catch (error) {
+    resHandler.setError(
+      HTTP_STATUS.UNAUTHORIZED,
+      RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN
+    );
+    logErrorResponse(req, req.id, RES_MESSAGES.AUTH.ERROR.INVALID_TOKEN);
+    return resHandler.send(res);
+  }
+};
