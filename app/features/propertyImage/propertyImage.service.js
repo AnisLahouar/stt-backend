346901755001d35233de41ff19f545941efc8d31
AnisLahouar <anislahouar98@gmail.com>
2025-05-23 00:55:12 +0100

    fix: missing path import

--- app/features/propertyImage/propertyImage.service.js
+++ app/features/propertyImage/propertyImage.service.js
@@ -1,6 +1,7 @@
 const { RES_MESSAGES } = require("../../core/variables.constants");
 const { PropertyImage } = require("../../database");
 const { compressImageBuffer, saveImageToDisk } = require("../../helpers/image.helper");
+const path = require('path');
 
 exports.createImageData = async (property, fileName, inTransaction) => {
   try {
@@ -18,17 +19,17 @@ exports.createImageData = async (property, fileName, inTransaction) => {
     }
   }
   catch (error) {
-    throw new Error(error.message)
+    throw new Error("Create Image Data Failed: " + error.message)
   }
 }
 
 exports.UploadFile = async (inImage, inPath, inName) => {
-  const compressedImage = await compressImageBuffer(inImage.buffer);
-
-  const imagePath = path.join(__dirname, `../../uploads/${inPath}`, `${inName}.jpg`);
-
-  const saved = await saveImageToDisk(compressedImage, imagePath);
-  if (!saved) {
-    throw new Error("Image Save to Disk Failed");
+  try {
+    const compressedImage = await compressImageBuffer(inImage.buffer);
+    const imagePath = path.join(__dirname, `../../uploads/${inPath}`, `${inName}.jpg`);
+    await saveImageToDisk(compressedImage, imagePath);
+  }
+  catch (e) {
+    throw new Error("Image Save to Disk Failed: " + e.message);
   }
 }
